fee51dc5c3006a1c59da268c5bebf8a50734188b
Wurmatron <wurmpleslater@gmail.com>
2017-02-25 23:00:15 -0500

    Fixed Warp Class Missing Dimension

--- src/main/java/wurmcraft/serveressentials/common/api/storage/Warp.java
+++ src/main/java/wurmcraft/serveressentials/common/api/storage/Warp.java
@@ -4,7 +4,7 @@ import net.minecraft.util.math.BlockPos;
 
 public class Warp extends Home {
 
-    public Warp(String name, BlockPos location, float yaw, float pitch) {
-        super(name, location, yaw, pitch);
+    public Warp(String name, BlockPos location,int dimension, float yaw, float pitch) {
+        super(name, location,dimension, yaw, pitch);
     }
 }
