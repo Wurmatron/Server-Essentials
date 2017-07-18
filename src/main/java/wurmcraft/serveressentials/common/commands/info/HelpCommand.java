efc14dc2e6cbe8292291f87aaf4b889027fc133b
Wurmatron <wurmatron@gmail.com>
2017-07-18 15:47:15 -0400

    Fixed Help Command Preformance Issue

--- src/main/java/wurmcraft/serveressentials/common/commands/info/HelpCommand.java
+++ src/main/java/wurmcraft/serveressentials/common/commands/info/HelpCommand.java
@@ -9,6 +9,7 @@ import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.event.ClickEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import wurmcraft.serveressentials.common.chat.ChatHelper;
 import wurmcraft.serveressentials.common.commands.utils.SECommand;
 import wurmcraft.serveressentials.common.reference.Local;
@@ -20,7 +21,8 @@ import java.util.Set;
 
 public class HelpCommand extends SECommand {
 
-	public static final int chatWidth = 54;
+	private static final int chatWidth = 54;
+	private static final Map <String, ICommand> prunedAliases = pruneAliases (FMLCommonHandler.instance ().getMinecraftServerInstance ().getCommandManager ());
 
 	public HelpCommand (Perm perm) {
 		super (perm);
@@ -36,18 +38,16 @@ public class HelpCommand extends SECommand {
 		return "/help <#>";
 	}
 
-	private static Map<String, ICommand> pruneAliases(final ICommandManager manager) {
-		return new HashMap<String, ICommand>() {
+	private static Map <String, ICommand> pruneAliases (final ICommandManager manager) {
+		return new HashMap <String, ICommand> () {
 			{
-				final Map<String, ICommand> commands = manager.getCommands();
-				Set<String> keySet = commands.keySet();
-				keySet.forEach((final String k1) -> {
-					keySet.forEach((final String k2) -> {
-						if (!k1.equalsIgnoreCase(k2) && !values().contains(commands.get(k1))) {
-							put(k1, commands.get(k1));
-						}
-					});
-				});
+				final Map <String, ICommand> commands = manager.getCommands ();
+				Set <String> keySet = commands.keySet ();
+				keySet.forEach ((final String k1) -> keySet.forEach ((final String k2) -> {
+					if (!k1.equalsIgnoreCase (k2) && !values ().contains (commands.get (k1))) {
+						put (k1,commands.get (k1));
+					}
+				}));
 			}
 		};
 	}
@@ -55,25 +55,25 @@ public class HelpCommand extends SECommand {
 	@Override
 	public void execute (MinecraftServer server,ICommandSender sender,String[] args) throws CommandException {
 		int start = 0;
-		final Map<String, ICommand> prunedAliases = pruneAliases(server.commandManager);
 		try {
 			if (args.length == 1 && Integer.parseInt (args[0]) != -1)
 				start = 8 * Integer.parseInt (args[0]);
-			if (start <= prunedAliases.size()) {
-				String nPage = " Page # ".replaceAll("#", "" + start / 8);
-				StringBuilder b = new StringBuilder();
-				int startPos = (int)Math.floor((chatWidth-nPage.length())/2);
-				b.append(Local.SPACER.substring(0, startPos-1) + nPage);
-				b.append(Local.SPACER.substring(0, chatWidth-b.length()));
-				if (start/8==0) ChatHelper.sendMessageTo(sender, TextFormatting.RED + b.toString());
-				else ChatHelper.sendMessageTo(sender, TextFormatting.RED + b.toString(), clickEvent((start/8)-1), null);
+			if (start <= prunedAliases.size ()) {
+				String nPage = " Page # ".replaceAll ("#","" + start / 8);
+				StringBuilder b = new StringBuilder ();
+				int startPos = (int) Math.floor ((chatWidth - nPage.length ()) / 2);
+				b.append (Local.SPACER.substring (0,startPos - 1) + nPage).append (Local.SPACER.substring (0,chatWidth - b.length ()));
+				if (start / 8 == 0)
+					ChatHelper.sendMessageTo (sender,TextFormatting.RED + b.toString ());
+				else
+					ChatHelper.sendMessageTo (sender,TextFormatting.RED + b.toString (),clickEvent ((start / 8) - 1),null);
 				for (int index = start; index < (start + 8); index++)
-					if (index < prunedAliases.size()) {
-						TextComponentTranslation temp = new TextComponentTranslation (formatCommand (sender,(ICommand) prunedAliases.values().toArray()[index]));
+					if (index < prunedAliases.size ()) {
+						TextComponentTranslation temp = new TextComponentTranslation (formatCommand (sender,(ICommand) prunedAliases.values ().toArray ()[index]));
 						temp.setStyle (new Style ().setColor (TextFormatting.DARK_AQUA));
 						sender.addChatMessage (temp);
 					}
-				ChatHelper.sendMessageTo (sender,TextFormatting.RED + Local.SPACER,clickEvent ((start / 8) + 1), null);
+				ChatHelper.sendMessageTo (sender,TextFormatting.RED + Local.SPACER,clickEvent ((start / 8) + 1),null);
 			}
 		} catch (NumberFormatException e) {
 			ChatHelper.sendMessageTo (sender,Local.INVALID_NUMBER.replaceAll ("#",args[0]));
