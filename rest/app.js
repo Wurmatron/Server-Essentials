d5af7ff772cd1cde47bbba0281dbc6511f004031
Wurmatron <wurmatron@gmail.com>
2018-06-27 17:17:31 -0400

    Added user API, Json Formatting

--- rest/app.js
+++ rest/app.js
@@ -19,13 +19,14 @@ const urlencoder = bodyParser.urlencoded({
 });
 app.use(bodyParser.json());
 app.use(morgan('short'));
+app.set('json spaces', 2);
 
 /**
  * Gets Rank from name
  *
  * 404 if rank name is not found
  */
-app.get('/rank/:name', (req, res) => {
+app.get('/rank/find/:name', (req, res) => {
     const rank = ranksDB.get(req.params.name);
     rank.then(function (result) {
         res.json(JSON.parse(result.toString('utf8')))
@@ -41,7 +42,7 @@ app.get('/rank/:name', (req, res) => {
  * 201 Created
  * 400 Invalid Request missing rank name
  */
-app.post('/rank', urlencoder, (req, res) => {
+app.post('/rank/add', urlencoder, (req, res) => {
     if (req.body.name) {
         const rank = ranksDB.get(req.body.name)
         rank.then(function (result) {
@@ -57,13 +58,41 @@ app.post('/rank', urlencoder, (req, res) => {
 });
 
 /**
+ * Returns a Array of all the ranks
+ *
+ * 304 Ranks sent
+ */
+app.get('/rank/find', urlencoder, (req, res) => {
+    var allRanks = []
+    ranksDB.createReadStream()
+        .on('data', function (data) {
+            const rankData = JSON.parse(data.value.toString('utf8'))
+            console.log(rankData)
+            allRanks.push({
+                name: rankData.name,
+                prefix: rankData.prefix,
+                suffix: rankData.suffix,
+                inheritance: rankData.inheritance,
+                permission: rankData.permission
+            })
+        })
+        .on('error', function (err) {
+            console.log('Oh my!', err)
+        })
+        .on('close', function () {})
+        .on('end', function () {
+            res.json(allRanks)
+        })
+})
+
+/**
  * Removes a rank from the database
  *
  * 200 Rank has been deleted
  * 404 Rank does not exist
  * 400 Invalid Request missing rank name
  */
-app.delete('/rank', urlencoder, (req, res) => {
+app.delete('/rank/delete', urlencoder, (req, res) => {
     if (req.body.name) {
         const rank = ranksDB.get(req.body.name)
         rank.then(function (result) {
@@ -84,7 +113,7 @@ app.delete('/rank', urlencoder, (req, res) => {
  * 400 Invalid Request missing rank name
  * 201 Rank Overridden / Created
  */
-app.put('/rank', urlencoder, (req, res) => {
+app.put('/rank/override', urlencoder, (req, res) => {
     if (req.body.name) {
         addRankEntry(req, res)
     } else {
@@ -112,6 +141,130 @@ function addRankEntry(req, res) {
     });
     res.sendStatus(201);
 }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=
+//        User
+// =-=-=-=-=-=-=-=-=-=-=-=-=
+/**
+ * Gets User from uuid
+ *
+ * 404 if uuid / player is not found
+ */
+app.get('/user/find/:uuid', (req, res) => {
+    const user =  userDB.get(req.params.uuid);
+    user.then(function (result) {
+        res.json(JSON.parse(result.toString('utf8')))
+    }, function (err) {
+        res.sendStatus(404);
+    })
+});
+
+/**
+ * Creates a new user entry from input json data
+ *
+ * 409 User already exists
+ * 201 Created
+ * 400 Invalid Request missing uuid
+ */
+app.post('/user/add', urlencoder, (req, res) => {
+    if (req.body.uuid) {
+        const user = userDB.get(req.body.uuid)
+        user.then(function (result) {
+            if (!user) {
+                addUserEntry(req, res)
+            } else {
+                res.sendStatus(409)
+            }
+        }, function (err) {
+            addUserEntry(req, res)
+        })
+    } else {
+        res.sendStatus(400)
+    }
+});
+
+/**
+ * Returns a Array of all the users
+ *
+ * 304 User sent
+ */
+app.get('/user/find', urlencoder, (req, res) => {
+    var allUsers = []
+    userDB.createReadStream()
+        .on('data', function (data) {
+            const userData = JSON.parse(data.value.toString('utf8'))
+            console.log(userData)
+            allUsers.push({
+                uuid: userData.uuid,
+                discord: userData.discord,
+            })
+        })
+        .on('error', function (err) {
+            console.log('Oh my!', err)
+        })
+        .on('close', function () {
+        })
+        .on('end', function () {
+            res.json(allUsers)
+        })
+})
+
+/**
+ * Removes a user from the database
+ *
+ * 200 User has been deleted
+ * 404 User does not exist
+ * 400 Invalid Request missing user
+ */
+app.delete('/user/delete', urlencoder, (req, res) => {
+    if (req.body.uuid) {
+        const user = userDB.get(req.body.uuid)
+        user.then(function (result) {
+            console.log("Removing entry '" + req.body.uuid + "'");
+            userDB.del(req.body.uuid);
+            res.sendStatus(200)
+        }, function (err) {
+            res.sendStatus(404)
+        })
+    } else {
+        res.sendStatus(400)
+    }
+});
+
+/**
+ * Overrides a current user or created a new one
+ *
+ * 400 Invalid Request missing rank name
+ * 201 User Overridden / Created
+ */
+app.put('/user/override', urlencoder, (req, res) => {
+    if (req.body.uuid) {
+        addUserEntry(req, res)
+    } else {
+        res.sendStatus(400)
+    }
+});
+
+/**
+ * Add an entry to the user DataBase
+ *
+ * 201 if user is added
+ * 400 Invalid request, missing user uuid
+ */
+function addUserEntry(req, res) {
+    console.log("Adding entry '" + req.body.uuid + "'");
+    userDB.put(req.body.uuid, JSON.stringify({
+        uuid: req.body.uuid,
+        discord: req.body.discord,
+        rank: req.body.rank,
+        permission: req.body.permission
+    }), function (err) {
+        if (err)
+            req.sendStatus(400);
+    });
+    res.sendStatus(201);
+}
+
 
 // Start NodeJS Server
 app.listen(port, () => {
