be39ffb4ea3a6cd7dfef85524712a84a6cc1062e
Wurmatron <wurmatron@gmail.com>
2018-11-07 20:43:43 -0500

    Added permission / admin bypass

--- src/main/java/com/wurmcraft/serveressentials/common/protect/ProtectionEvents.java
+++ src/main/java/com/wurmcraft/serveressentials/common/protect/ProtectionEvents.java
@@ -1,6 +1,7 @@
 package com.wurmcraft.serveressentials.common.protect;
 
 import com.wurmcraft.serveressentials.api.protection.Town;
+import com.wurmcraft.serveressentials.common.utils.CommandUtils;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -10,8 +11,10 @@ public class ProtectionEvents {
   public void blockBreak(BreakEvent e) {
     if (ProtectionModule.isAreaClaimed(e.getPos())) {
       Town town = ProtectionModule.getTownForPos(e.getPos());
-      if (!town.getOwnerID().equalsIgnoreCase(e.getPlayer().getGameProfile().getId().toString()))
+      if (!town.getOwnerID().equalsIgnoreCase(e.getPlayer().getGameProfile().getId().toString())
+          || CommandUtils.hasPerm("protection.bypass", e.getPlayer())) {
         e.setCanceled(true);
+      }
     }
   }
 }
